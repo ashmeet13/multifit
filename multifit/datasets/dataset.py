3faa0b2a4f1e4e95f98c8b008e9976c83a54cef2
ashmeet13 <ashmeet.l13@gmail.com>
2019-12-12 20:01:22 +0530

    Dataset.py regex change

--- multifit/datasets/dataset.py
+++ multifit/datasets/dataset.py
@@ -6,14 +6,15 @@ from fastai_contrib.text_data import MosesPreprocessingFunc, \
 
 def read_wiki_articles(filename):
     def istitle(line):
-        return line.startswith('Section')
+        line = line.strip()
+        return line.startswith('=') or line.endswith('=')
     articles = []
     with open(filename, encoding='utf8') as f:
         lines = f.readlines()
     current_article = []
     for i, line in enumerate(lines):
         current_article.append(line)
-        if i < len(lines) - 2 and lines[i + 1].strip() == "" and istitle(lines[i + 2]):
+        if istitle(line):
             articles.append("".join(current_article))
             current_article = []
     articles.append("".join(current_article))
