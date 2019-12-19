ebebd41982bba3c3af349dd15811333838f95403
ashmeet13 <ashmeet.l13@gmail.com>
2019-12-20 00:48:40 +0530

    changes

--- multifit/datasets/dataset.py
+++ multifit/datasets/dataset.py
@@ -213,14 +213,16 @@ class ULMFiTDataset(Dataset):
         super().__post_init__()
         self._vocab = None
 
-    def load_lm_databunch(self, bs, bptt):
+    def load_lm_databunch(self, bs, bptt, set_vocab=None):
         lm_suffix = str(bptt) if bptt != 70 else ""
         lm_suffix += "" if self.use_tst_for_lm else "-notst"
+        args = dict(vocab=set_vocab)
         data_lm = self.load_n_cache_databunch(f"lm{lm_suffix}",
                                               bunch_class=TextLMDataBunch,
                                               data_loader=self.load_unsupervised_data,
                                               bptt=bptt,
-                                              bs=bs)
+                                              bs=bs,
+                                              **args)
 
         with (self.cache_path / "itos.pkl").open('wb') as f:
             pickle.dump(data_lm.vocab.itos, f)
@@ -254,6 +256,7 @@ class ULMFiTDataset(Dataset):
         return data_cls
 
     def load_n_cache_databunch(self, name, bunch_class, data_loader, bs, **args):
+        print("This is name - ",name)
         bunch_path = self.cache_path / name
         databunch = None
         if bunch_path.exists():
@@ -271,6 +274,7 @@ class ULMFiTDataset(Dataset):
 
     def databunch_from_df(self, bunch_class, train_df, valid_df, **args):
         args.update(**self.tokenizer.get_fastai_config(dataset_uses_moses=self.uses_moses))  # TODO depends on the previous model
+        print(args)
         databunch = make_data_bunch_from_df(cls=bunch_class,
                                             path=self.cache_path,
                                             train_df=train_df,
@@ -347,12 +351,14 @@ class ULMFiTTokenizer:
 
     def _get_processor_sentence_piece(self, ds_uses_moses):
         moses_preproc = [MosesPreprocessingFunc(self.arch.lang)] if not ds_uses_moses else []
-
+        print(self.pretrained_path)
         sp_model = self.pretrained_path / 'spm.model'
         if not sp_model.is_file():
+            print("yesss")
             sp_model = None
         sp_vocab = self.pretrained_path / 'spm.vocab'
         if not sp_vocab.is_file():
+            print("yesss")
             sp_vocab = None
         processor = SPProcessor2(
             pre_rules=moses_preproc + defaults.text_pre_rules,
--- multifit/training.py
+++ multifit/training.py
@@ -253,6 +253,40 @@ class ULMFiTPretraining(ULMFiTTrainingCommand):
         learn.unfreeze()
         learn.fit_one_cycle(self.num_epochs, self.lr, (0.8, 0.7))
 
+    def train_custom(self, dataset_or_path, tokenizer=None, set_vocab=None, continue_pretrain=None, **train_config):
+        if self.arch.lang is None:
+            lang = detect_lang_from_dataset_path(Path(dataset_or_path))
+            if lang is None:
+                warn("Unable to detect language from dataset path assuming English, use replace_(lang='??') change it.")
+                lang = 'en'
+            self.arch.lang = lang
+        self.replace_(**train_config, _strict=True)
+        set_seed(self.seed, "LM weights seed")
+        if tokenizer is None:
+            if hasattr(self, 'base'):
+                tokenizer = self.base.tokenizer
+            else:
+                tokenizer = self.arch.new_tokenizer()
+
+        dataset = self._set_dataset_(dataset_or_path, tokenizer)
+        learn = self.get_learner(data_lm=dataset.load_lm_databunch(bs=self.bs, bptt=self.bptt, set_vocab=set_vocab))
+        if continue_pretrain is not None:
+            learn.load(continue_pretrain)
+
+        experiment_path = learn.path / learn.model_dir
+        print("Experiment", experiment_path)
+        if self.num_epochs > 0:
+            self._fit_schedule(learn)
+
+        self.experiment_path = experiment_path
+        tokenizer.save(self.experiment_path, learn=learn)
+        learn.to_fp32()
+        learn.save_encoder(ENC_BEST)
+        learn.save(LM_BEST, with_opt=False)
+        learn.destroy()
+        self.save_paramters()
+        print("Language model saved to", self.experiment_path)
+
     def train_(self, dataset_or_path, tokenizer=None, **train_config):
         if self.arch.lang is None:
             lang = detect_lang_from_dataset_path(Path(dataset_or_path))
